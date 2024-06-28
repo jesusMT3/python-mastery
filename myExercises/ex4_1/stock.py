877a94e0cbd46722f10ec367780c222edab192ab
Jesus Marcos Torero <jesusmarcostorero@gmail.com>
2024-06-28 14:30:42 +0200

    reformat myExercides folder

--- myExercises/ex4_1/stock.py
+++
@@ -1,8 +0,0 @@
-class SimpleStock:
-    def __init__(self, name, shares, price):
-        self.name = name
-        self.shares = shares
-        self.price = price
-    
-    def cost(self):
-        return self.price * self.shares
