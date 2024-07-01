5b412bd5f5c9a85ae3dbca396e70ab761e588643
Jesus Marcos Torero <jesusmarcostorero@gmail.com>
2024-07-01 14:18:44 +0200

    ex6_1

---
+++ myExercises/structure.py
@@ -0,0 +1,17 @@
+class Structure:
+    _fields = ()
+
+    def __init__(self, *args):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected %d arguments' % len(self._fields))
+        for name, arg in zip(self._fields,args):
+            setattr(self, name, arg)
+            
+    def __repr__(self):
+        return f"{type(self.__name__)}({', '.join(repr(getattr(self, name)) for name in self._fields)})"
+    
+    def __setattr__(self, name, value):
+        if name.startswith('_') or name in self._fields:
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError('No attribute %s' % name)
